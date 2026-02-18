d1515ad50457af0cc69ebb17e3a2700a0f0b68e8
govindgcoder <govind.g.coder@gmail.com>
2026-02-18 20:36:21 +0530

    added new line

--- src/SynchronizationDemo.java
+++ src/SynchronizationDemo.java
@@ -1,36 +1,45 @@
 class Table {
+
     // TODO: synchronized method void printTable(int n)
     // Loop from 1 to 5 (as per sample) or 10
     // Print n * i + " "
     // Handle InterruptedException (try-catch Thread.sleep(400))
-    public void printTable(int n){
-        for(int i=1; i<=5; i++){
-            System.out.print(n*i+" ");
+    public void printTable(int n) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(n * i + " ");
         }
+        System.out.print("\n");
     }
 }
 
 class MyThread1 extends Thread {
+
     Table t;
+
     MyThread1(Table t) {
         this.t = t;
     }
+
     public void run() {
         t.printTable(5);
     }
 }
 
 class MyThread2 extends Thread {
+
     Table t;
+
     MyThread2(Table t) {
         this.t = t;
     }
+
     public void run() {
         t.printTable(100);
     }
 }
 
 public class SynchronizationDemo {
+
     public static void main(String[] args) {
         // Create Table object
         Table tb = new Table();
@@ -39,7 +48,6 @@ public class SynchronizationDemo {
         MyThread2 m2 = new MyThread2(tb);
         // TODO: Start both threads
         m1.run();
-        System.out.print("\n");
         m2.run();
     }
 }
